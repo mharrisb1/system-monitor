cb558e2c48ef3a003ed8441afa18fe2194a8d57d
Michael Harris <michael.harris@block.one>
2020-05-13 18:33:06 -0400

    Add format hour test

--- test/format.test.cpp
+++ test/format.test.cpp
@@ -1,2 +1,8 @@
 #include "gtest/gtest.h"
+#include "../include/format.h"
 
+TEST(ExampleTests, FormatTime) {
+    std::string result{Format::ElapsedTime(752)};
+    EXPECT_EQ("00:12:32",
+              result);
+}
