fc7a17f411ba4c375c3250c6e61e13dac1cb2bce
Michael Harris <michael.harris@block.one>
2020-05-12 23:28:28 -0400

    implementation of Format::ElapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <array>
+#include <charconv>
 
 #include "format.h"
 
@@ -7,5 +9,28 @@ using std::string;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    long int    hour,
+                min;
+
+    std::string hh,
+                mm,
+                ss;
+
+    // hours
+    seconds %= (24 * 60 * 60);
+    hour =     seconds / 3600;
+
+    // minutes
+    seconds %= 3600;
+    min =      seconds / 60;
+
+    // seconds
+    seconds %= 60;
+
+    hh = std::to_string(hour);
+    mm = std::to_string(min);
+    ss = std::to_string(seconds);
+
+    return hh + ':' + mm + ':' + ss;
+}
