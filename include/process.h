cac31a369ed7522929f906757771a315646d4421
Michael Harris <michael.harris@block.one>
2020-05-26 22:31:50 -0400

    respond to code review edits

--- include/process.h
+++ include/process.h
@@ -6,12 +6,12 @@
 class Process {
 public:
   explicit Process(int pid) : pid_(pid) {}
-  [[nodiscard]] int Pid() const { return pid_; }
-  [[nodiscard]] std::string User() const;
-  [[nodiscard]] std::string Command() const;
-  [[nodiscard]] float CpuUtilization() const;
-  [[nodiscard]] std::string Ram() const;
-  [[nodiscard]] long int UpTime() const;
+  int Pid() const { return pid_; }
+  std::string User() const;
+  std::string Command() const;
+  float CpuUtilization() const;
+  std::string Ram() const;
+  long int UpTime() const;
 
 private:
   int pid_{0};
